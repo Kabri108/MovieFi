c823e843e03c1db89676b856943c481d10b0fa69
Sayantan Kabri <sayantankabri108@gmail.com>
2024-09-10 20:01:50 +0530

    baseurl change

--- client/src/Redux/APIs/Axios.jsx
+++ client/src/Redux/APIs/Axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Axios=axios.create({
     // baseURL:'http://localhost:5000/api',
-    baseURL:process.env.VERCEL_URL
+    baseURL:'https://movie-fi-dun.vercel.app/api'
 })
 
 export default Axios;
